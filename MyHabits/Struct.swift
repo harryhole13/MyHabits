313e97c5e11796ac2849ebf631d068b9d48d05f8
harryhole13 <harryhole13@gmail.com>
2022-10-10 00:42:20 +0300

    Del last Habit

---
+++ MyHabits/Struct.swift
@@ -0,0 +1,20 @@
+//
+//  Struct.swift
+//  MyHabits
+//
+//  Created by Алексей Потемин on 25.09.2022.
+//
+
+import Foundation
+
+struct InfoHabits {
+    let author: String
+    let description: String
+    let image: String
+    let likes: Int
+    let views: Int
+}
+
+struct ModelInfo {
+        let text: String
+}
